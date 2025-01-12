ece8d07579698ac296458269a11cf4080037a005
putnik13 <ilrum1398@gmail.ru>
2025-01-12 14:44:53 +0300

    Начало серьезной работы над центральным блоком управления. Реализовано получение, анализ, сохранение и проверка целостности данных от модулей с датчиками

--- program/main_module/main_module.ino
+++ program/main_module/main_module.ino
@@ -1,56 +1,235 @@
 #include <SPI.h>
 #include <RF24.h>
+#include <LiquidCrystal_I2C.h>
+
 #include <string.h>
-#define CHANNEL_NUMBER 8
-#define SIZE_TMD_DATA 8
-#define CENTRAL_MODULE_ID      0
-#define INTERNAL_MODULE_ID     1
-#define EXTERNAL_MODULE_ID     2
-#define PIPE_READ_ADDRESS          0xF0F0F0F0E1LL
-#define PIPE_WRITE_ADDRESS         0xF0F0F0F0E2LL
+
+#define LOOP_DELAY_MSEC            10
+#define PIPE_READ_ADDRESS          0xF0F0F0F0E2LL
+#define PIPE_WRITE_ADDRESS         0xF0F0F0F0E1LL
 #define SERIAL_SPEED               9600
 #define RADIO_CHANNEL_NUMBER       8
 #define DATA_SEGMENT_LENGTH        8
-#define DATA_SEGMENT_LENGTH_B      DATA_SEGMENT_LENGTH*sizeof(float)  //32
+#define DATA_SEGMENT_LENGTH_B      DATA_SEGMENT_LENGTH * sizeof(float)  //32
 #define COUNT_SEGMENTS_IN_PACKET   3
-RF24 radio(9, 10);  // указать номера пинов, куда подключаются CE и CSN
+
+#define CENTRAL_MODULE_ID      0
+#define INTERNAL_MODULE_ID     1
+#define EXTERNAL_MODULE_ID     2
+#define INCORRECT_MODULE_ID    3
+
+#define RADIO_CE_PORT          8
+#define RADIO_CSN_PORT         9
+#define LCD2004_ADDRESS        0x3F
+
+enum TYPE_PACKET{
+  DATA    = 1,
+  CONTROL = 2,
+  SERVICE = 3,
+  UNKNOWN = 4
+};
+enum COMMANDS_TYPE{
+  RESTART_ALL          = 1,
+  TURNOFF_RADIO        = 2,
+  CHANGE_SEND_INTERVAL = 3,
+  STOP_SEND_DATA       = 4,
+  RESUME_SEND_DATA     = 5,
+  GET_DETECTOR_MAP     = 6,
+  GET_TIME_INTERVAL    = 7,
+  GET_LIFE_TIME        = 8, //в миллисек.
+  HEARTBEAT            = 9
+};
+
+enum SERVICE_MSG_TYPE{
+  START_MODULE_SUCCESS = 1,
+  SUCCESS_GET_COMMAND  = 2,
+  ERROR_START_DETECTOR = 3,
+  REPORT_DETECTOR_MAP  = 4,
+  REPORT_TIME_INTERVAL = 5,
+  REPORT_LIFE_TIME     = 6,
+  GET_ERROR_COMMAND    = 7
+};
+
+LiquidCrystal_I2C lcd(LCD2004_ADDRESS, 20, 4);
+RF24 radio(RADIO_CE_PORT, RADIO_CSN_PORT);
+
+float currIncomingData[DATA_SEGMENT_LENGTH];
+float servicePacketExternal[DATA_SEGMENT_LENGTH];
+float servicePacketInternal[DATA_SEGMENT_LENGTH];
+float actionPacket[DATA_SEGMENT_LENGTH];
+float** dataPacketExternal = nullptr;
+float** dataPacketInternal = nullptr;
+TYPE_PACKET currPacketType = TYPE_PACKET::UNKNOWN;
+int currPacketModuleId = INCORRECT_MODULE_ID;
+
 //Здесь сырые данные должны переводиться в адекватные единицы
-float tmdData[SIZE_TMD_DATA];
-char dataParam[10];
-void setup(){                        
-    radio.begin();
-    radio.setChannel(CHANNEL_NUMBER);
-    radio.setDataRate(RF24_1MBPS);
-    radio.setPALevel(RF24_PA_HIGH);
-    radio.openReadingPipe(1,0x1234567890LL);
-    radio.startListening();
-    Serial.begin(9600);
+void setup()
+{
+  Serial.begin(SERIAL_SPEED);
 
-}
-void loop(){
-  if (radio.available())
-  {
-    radio.read(&tmdData, SIZE_TMD_DATA*sizeof(float));   
-    
-    for(int i=0;i<SIZE_TMD_DATA;++i){
-      sprintf(dataParam, "%d: %f", i, tmdData[i]);
-      Serial.println(dataParam);
+  if(!dataPacketExternal){
+    dataPacketExternal = new float*[COUNT_SEGMENTS_IN_PACKET];
+    for(int i = 0; i < COUNT_SEGMENTS_IN_PACKET; i++){
+      dataPacketExternal[i] = new float[DATA_SEGMENT_LENGTH];
     }
-    if((int)tmdData[7]==calcCheckSum(tmdData,SIZE_TMD_DATA)){
-      Serial.println("Packet is valid");  
-    }else{
-      Serial.println("!!Packet is invalid!!"); 
-    }      
-  }else{
-    Serial.println("No data");
   }
-  delay(20);
+
+  if(!dataPacketInternal){
+    dataPacketInternal = new float*[COUNT_SEGMENTS_IN_PACKET];
+    for(int i = 0; i < COUNT_SEGMENTS_IN_PACKET; i++){
+      dataPacketInternal[i] = new float[DATA_SEGMENT_LENGTH];
+    }
+  }
+  
+  startDisplay();
+  startRadio();   
 }
 
-int calcCheckSum(float* data, int sizeData){
-  int sum=0;
-  for(int i=0;i<sizeData-1;i++){
-    sum+=(int)data[i];
+void startDisplay()
+{
+  lcd.init();                       // Инициируем работу с LCD дисплеем
+  lcd.backlight();                  // Включаем подсветку LCD дисплея
+  lcd.setCursor(0, 0);              // Устанавливаем курсор в позицию (0 столбец, 0 строка)
+  lcd.print("LCD");                 // Выводим текст "LCD", начиная с установленной позиции курсора
+  lcd.setCursor(0, 2);              // Устанавливаем курсор в позицию (0 столбец, 1 строка)
+  lcd.print("www.iarduino.ru");
+}
+
+void startRadio()
+{
+  radio.begin();
+  radio.setChannel(RADIO_CHANNEL_NUMBER);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.openReadingPipe(1, PIPE_READ_ADDRESS);
+  radio.openWritingPipe(PIPE_WRITE_ADDRESS); //Открываем трубу для отправки
+  radio.startListening();
+}
+
+void loop(){
+  if (!radio.available())
+  {
+    //Serial.println("No data");
+    goto end_loop;
   }
-  return sum;   
+    
+  Serial.println("Data Incoming!");
+  radio.read(currIncomingData, DATA_SEGMENT_LENGTH_B);
+
+  for(int i = 0; i < DATA_SEGMENT_LENGTH; i++){
+    Serial.println(i + (String)": " + currIncomingData[i]);
+  }
+
+  const bool validIncomingData = analyzeIncomingPacket(currIncomingData);
+
+  if(!validIncomingData){
+    Serial.println("DATA IS INVALID!");
+    goto end_loop;
+  }
+
+  saveIncomingData(currIncomingData);
+
+  if(currPacketType == TYPE_PACKET::DATA){
+    //Если это сегмент пакета данных, анализ проводим только после получения сегмента 2, 
+    //иначе будет попытка работы с неполным пакетом данных
+    if(currIncomingData[6] != (COUNT_SEGMENTS_IN_PACKET - 1)){
+      goto end_loop;
+    }
+  }
+    
+  if(checkIncomingDataIntegrity()){
+    Serial.println("Packet is Correct!");
+    debugSavedIncomingPacket();
+  }else{
+    Serial.println("!!Packet is Corrupted!!");
+    goto end_loop_with_reset;
+  }
+
+  //TODO Использование полученных и проверенных данных
+
+end_loop_with_reset:
+  resetIncomingDataBuffers();
+end_loop:
+  delay(LOOP_DELAY_MSEC);
+}
+
+bool analyzeIncomingPacket(float* packet)
+{
+  if(packet[0] != CENTRAL_MODULE_ID) 
+    return false; //Проверка, что данный пакет предназначен главному модулю
+
+  if(packet[1] == INTERNAL_MODULE_ID || packet[1] == EXTERNAL_MODULE_ID)
+  {
+    currPacketModuleId = static_cast<int>(packet[1]);
+  }
+  else
+  {
+    currPacketModuleId = INCORRECT_MODULE_ID;
+    return false;
+  }
+
+  if(packet[2] == TYPE_PACKET::DATA || packet[2] == TYPE_PACKET::SERVICE)
+  {
+    currPacketType = static_cast<int>(packet[2]);
+  }
+  else
+  {
+    currPacketType = TYPE_PACKET::UNKNOWN;
+    return false;
+  }
+
+  return true;
+}
+
+//Сохраняем данные в нужный буфер в зависимости от результатов проведенного ранее анализа
+void saveIncomingData(float* packet)
+{
+  if(currPacketModuleId == INTERNAL_MODULE_ID){
+    if(currPacketType == TYPE_PACKET::DATA){
+      memmove(dataPacketInternal[static_cast<int>(packet[6])], packet, DATA_SEGMENT_LENGTH_B);
+    }else if(currPacketType == TYPE_PACKET::SERVICE){
+      memmove(servicePacketInternal, packet, DATA_SEGMENT_LENGTH_B);
+    }
+  }else if(currPacketModuleId == EXTERNAL_MODULE_ID){
+    if(currPacketType == TYPE_PACKET::DATA){
+      memmove(dataPacketExternal[static_cast<int>(packet[6])], packet, DATA_SEGMENT_LENGTH_B);
+    }else if(currPacketType == TYPE_PACKET::SERVICE){
+      memmove(servicePacketExternal, packet, DATA_SEGMENT_LENGTH_B);
+    }
+  }
+}
+
+//Проверяем целостность сохраненных данных, вычисляя контрольную сумму
+bool checkIncomingDataIntegrity()
+{
+  if(currPacketModuleId == INTERNAL_MODULE_ID){
+    if(currPacketType == TYPE_PACKET::DATA){
+      const float checkSum = calcFullCheckSum(dataPacketInternal, DATA_SEGMENT_LENGTH);
+      return (checkSum == dataPacketInternal[0][7] && checkSum == dataPacketInternal[1][7] && checkSum == dataPacketInternal[2][7]);
+    }else if(currPacketType == TYPE_PACKET::SERVICE){
+      return calcCheckSum(servicePacketInternal, DATA_SEGMENT_LENGTH) == servicePacketInternal[7];
+    }
+  }else if(currPacketModuleId == EXTERNAL_MODULE_ID){
+    if(currPacketType == TYPE_PACKET::DATA){
+      const float checkSum = calcFullCheckSum(dataPacketExternal, DATA_SEGMENT_LENGTH);
+      return (checkSum == dataPacketExternal[0][7] && checkSum == dataPacketExternal[1][7] && checkSum == dataPacketExternal[2][7]);
+    }else if(currPacketType == TYPE_PACKET::SERVICE){
+      return calcCheckSum(servicePacketExternal, DATA_SEGMENT_LENGTH) == servicePacketExternal[7];
+    }
+  }
+
+  return false;
+}
+
+void resetIncomingDataBuffers()
+{
+  for(int i = 0; i < COUNT_SEGMENTS_IN_PACKET; i++){
+    memset(dataPacketInternal[i], 0, DATA_SEGMENT_LENGTH_B);
+  }
+  for(int i = 0; i < COUNT_SEGMENTS_IN_PACKET; i++){
+    memset(dataPacketExternal[i], 0, DATA_SEGMENT_LENGTH_B);
+  }
+
+  memset(servicePacketInternal, 0, DATA_SEGMENT_LENGTH_B);
+  memset(servicePacketExternal, 0, DATA_SEGMENT_LENGTH_B);
 }
---
+++ program/main_module/service_funcs.ino
@@ -0,0 +1,79 @@
+#ifndef SERVICE_FUNCS
+#define SERVICE_FUNCS
+
+//Служебные функции
+
+float normalize(int value){
+  return value/1024.0;  
+}
+
+float calcCheckSum(float* data, int sizeData){
+  float sum=0;
+  for(int i=0;i<sizeData-1;i++){
+    sum += data[i]; //Можно считать с дробями т.к. на обоих модулях алгоритм подсчета одинаков и числа должны совпасть
+  }
+  return sum;   
+}
+
+float calcFullCheckSum(float** dataArray, int sizeData)
+{
+  return calcCheckSum(dataArray[0], sizeData) + calcCheckSum(dataArray[1], sizeData) + calcCheckSum(dataArray[2], sizeData);
+}
+
+void debugSavedIncomingPacket()
+{
+  if(currPacketModuleId == INTERNAL_MODULE_ID){
+    Serial.println("=== INTERNAL PACKET ===");
+    if(currPacketType == TYPE_PACKET::DATA){
+      debugDataPacket(dataPacketInternal);
+    }else if(currPacketType == TYPE_PACKET::SERVICE){
+      debugServicePacket(servicePacketInternal);
+    }
+  }else if(currPacketModuleId == EXTERNAL_MODULE_ID){
+    Serial.println("=== EXTERNAL PACKET ===");
+    if(currPacketType == TYPE_PACKET::DATA){
+      debugDataPacket(dataPacketExternal);
+    }else if(currPacketType == TYPE_PACKET::SERVICE){
+      debugServicePacket(servicePacketExternal);
+    }
+  }
+}
+
+void debugDataPacket(float** packet){
+  Serial.println("=== DATA PACKET ===");
+  for(int i = 0; i < COUNT_SEGMENTS_IN_PACKET; i++){
+    for(int j = 0; j < DATA_SEGMENT_LENGTH; j++){
+      Serial.print(packet[i][j] + (String)" | ");
+    }
+    Serial.println("\n------");
+  }
+}
+
+void debugServicePacket(float* packet){
+  Serial.println("=== SERVICE PACKET ===");
+  for(int j = 0; j < DATA_SEGMENT_LENGTH; j++){
+      Serial.print(packet[j] + (String)" | ");
+  }
+  Serial.print("\n");
+}
+
+void debugActionPacket(float* packet){
+  Serial.println("=== ACTION PACKET ===");
+  for(int j = 0; j < DATA_SEGMENT_LENGTH; j++){
+      Serial.print(packet[j] + (String)" | ");
+  }
+  Serial.print("\n");
+}
+
+void fillIncomingActionPacket(float* incomingPacket, int module_id, COMMANDS_TYPE command, float paramValue){
+  incomingPacket[0] = module_id;
+  incomingPacket[1] = CENTRAL_MODULE_ID;
+  incomingPacket[2] = TYPE_PACKET::CONTROL;
+  incomingPacket[3] = command;
+  incomingPacket[4] = paramValue;
+  incomingPacket[5] = 0;
+  incomingPacket[6] = 0;
+  incomingPacket[7] = calcCheckSum(incomingPacket, DATA_SEGMENT_LENGTH);
+}
+
+#endif
