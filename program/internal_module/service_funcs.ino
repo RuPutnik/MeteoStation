ed5652a5a662d744d67c9c093c30382b1c177a25
putnik13 <ilja.rumjancev@lenta.ru>
2025-02-22 11:30:45 +0300

    Прошивка для внутреннего модуля также переведена на новую систему передачи/приёма данных

--- program/internal_module/service_funcs.ino
+++ program/internal_module/service_funcs.ino
@@ -1,60 +1,46 @@
 //Служебные функции
-void resetDetectorMap(bool* detectorMap){
-  for(int i=0;i<COUNT_DETECTOR;i++){
-    detectorMap[i] = true;
-  }
-}
-
-float normalize(int value){
-  return value / 1024.0;  
-}
-
-float calcCheckSum(float* data, int sizeData){
-  float sum = 0;
-  for(int i=0;i<sizeData-1;i++){
-    sum += data[i]; //Можно считать с дробями т.к. на обоих модулях алгоритм подсчета одинаков и числа должны совпасть
-  }
-  return sum;   
-}
-
-float calcFullCheckSum(float** dataArray, int sizeData)
-{
-  return calcCheckSum(dataArray[0], sizeData) + calcCheckSum(dataArray[1], sizeData) + calcCheckSum(dataArray[2], sizeData);
-}
-
-void debugDataPacket(float** packet){
+void debugDataPacket(MeteoDataPacket* packet){
   Serial.println("=== DATA PACKET ===");
-  for(int i = 0; i < COUNT_SEGMENTS_IN_PACKET; i++){
-    for(int j = 0; j < DATA_SEGMENT_LENGTH; j++){
-      Serial.print(packet[i][j] + (String)" | ");
-    }
-    Serial.println("\n------");
-  }
+  Serial.println(static_cast<String>("type = ") + packet->type);
+  Serial.println(static_cast<String>("dest = ") + packet->dest);
+  Serial.println(static_cast<String>("sender = ") + packet->sender);
+  Serial.println(static_cast<String>("numPacket = ") + packet->numPacket);
+  Serial.println(static_cast<String>("valueParam1 = ") + packet->val1);
+  Serial.println(static_cast<String>("valueParam2 = ") + packet->val2);
+  Serial.println(static_cast<String>("valueParam3 = ") + packet->val3);
+  Serial.println(static_cast<String>("valueParam4 = ") + packet->val4);
+  Serial.println(static_cast<String>("valueParam5 = ") + packet->val5);
+  Serial.println(static_cast<String>("valueParam6 = ") + packet->val6);
+  Serial.println(static_cast<String>("ckSum = ") + packet->ckSum);
 }
 
-void debugServicePacket(float* packet){
+void debugServicePacket(ActionServicePacket* packet){
   Serial.println("=== SERVICE PACKET ===");
-  for(int j = 0; j < DATA_SEGMENT_LENGTH; j++){
-      Serial.print(packet[j] + (String)" | ");
-  }
-  Serial.print("\n");
+  Serial.println(static_cast<String>("type = ") + packet->type);
+  Serial.println(static_cast<String>("dest = ") + packet->dest);
+  Serial.println(static_cast<String>("sender = ") + packet->sender);
+  Serial.println(static_cast<String>("numPacket = ") + packet->numPacket);
+  Serial.println(static_cast<String>("serviceId = ") + packet->id);
+  Serial.println(static_cast<String>("valueParam = ") + packet->valueParam);
+  Serial.println(static_cast<String>("ckSum = ") + packet->ckSum);
 }
 
-void debugActionPacket(float* packet){
+void debugActionPacket(ActionServicePacket* packet){
   Serial.println("=== ACTION PACKET ===");
-  for(int j = 0; j < DATA_SEGMENT_LENGTH; j++){
-      Serial.print(packet[j] + (String)" | ");
-  }
-  Serial.print("\n");
+  Serial.println(static_cast<String>("type = ") + packet->type);
+  Serial.println(static_cast<String>("dest = ") + packet->dest);
+  Serial.println(static_cast<String>("sender = ") + packet->sender);
+  Serial.println(static_cast<String>("numPacket = ") + packet->numPacket);
+  Serial.println(static_cast<String>("actionId = ") + packet->id);
+  Serial.println(static_cast<String>("valueParam = ") + packet->valueParam);
+  Serial.println(static_cast<String>("ckSum = ") + packet->ckSum);
 }
 
-void fillIncomingActionPacket(float* incomingPacket, COMMANDS_TYPE command, float paramValue){
-  incomingPacket[0] = MODULE_ID::INTERNAL_MODULE_ID;
-  incomingPacket[1] = MODULE_ID::CENTRAL_MODULE_ID;
-  incomingPacket[2] = TYPE_PACKET::CONTROL;
-  incomingPacket[3] = command;
-  incomingPacket[4] = paramValue;
-  incomingPacket[5] = 0;
-  incomingPacket[6] = 0;
-  incomingPacket[7] = calcCheckSum(incomingPacket, DATA_SEGMENT_LENGTH);
+void fillIncomingActionPacket(ActionServicePacket* incomingPacket, COMMANDS_TYPE command, float paramValue){
+  incomingPacket->dest = MODULE_ID::INTERNAL_MODULE_ID;
+  incomingPacket->sender = MODULE_ID::CENTRAL_MODULE_ID;
+  incomingPacket->type = TYPE_PACKET::CONTROL;
+  incomingPacket->id = command;
+  incomingPacket->valueParam = paramValue;
+  incomingPacket->ckSum = calcCheckSum(incomingPacket, ACTSERV_PACKET_LENGTH);
 }
